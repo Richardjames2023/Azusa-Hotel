29cac1d34c1e174d8d0b89d9924fce9ae7af3822
Richard James <richardjames2023@gmail.com>
2026-09-18 12:30:35 +0100

    fix(deploy): rename underscore build directory to bypass cPanel FTP restrictions

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'export',      
+  distDir: 'dist',        
   images: {
-    unoptimized: true,
+    unoptimized: true,    
   },
 };
 
 module.exports = nextConfig;
+
